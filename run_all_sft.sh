8311e9d01ffe907703746c8dd5f59f944f978440
thucuyenyah <uyenthuc09012004@email.com>
2026-01-10 11:13:13 +0700

    fix sh

--- run_all_sft.sh
+++ run_all_sft.sh
@@ -7,9 +7,9 @@ export CUDA_VISIBLE_DEVICES=6
 # # ===============================
 # # Qwen 0.5B
 # # ===============================
-# bash run_all.sh 1 1 4 1   # hh
-# bash run_all.sh 1 2 4 1   # shp
-# bash run_all.sh 1 3 4 1   # pku
+bash run_all.sh 1 1 4 1   # hh
+bash run_all.sh 1 2 4 1   # shp
+bash run_all.sh 1 3 4 1   # pku
 
 # ===============================
 # TinyLLaMA 1.1B
@@ -21,8 +21,8 @@ bash run_all.sh 2 3 4 1   # pku
 # # ===============================
 # # Qwen 3B
 # # ===============================
-# bash run_all.sh 3 1 4 1   # hh
-# bash run_all.sh 3 2 4 1   # shp
-# bash run_all.sh 3 3 4 1   # pku
+bash run_all.sh 3 1 4 1   # hh
+bash run_all.sh 3 2 4 1   # shp
+bash run_all.sh 3 3 4 1   # pku
 
 echo "🎉 ALL JOBS FINISHED"
