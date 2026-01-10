87a4abafad088e822eab75bdeddb050aad8d2e84
thucuyenyah <uyenthuc09012004@email.com>
2026-01-10 11:36:28 +0700

    fix sh

--- run_all_sft.sh
+++ run_all_sft.sh
@@ -2,27 +2,32 @@
 set -e
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=6
 
-# # ===============================
-# # Qwen 0.5B
-# # ===============================
-bash run_all.sh 1 1 4 1   # hh
-bash run_all.sh 1 2 4 1   # shp
-bash run_all.sh 1 3 4 1   # pku
+# ===============================
+# Qwen 0.5B
+# ===============================
+CUDA_VISIBLE_DEVICES=6 bash run_all.sh 1 1 4 1 &   # hh
+CUDA_VISIBLE_DEVICES=5 bash run_all.sh 1 2 4 1 &   # shp
+CUDA_VISIBLE_DEVICES=1 bash run_all.sh 1 3 4 1 &   # pku
+
+wait
 
 # ===============================
 # TinyLLaMA 1.1B
 # ===============================
-bash run_all.sh 2 1 4 1   # hh
-bash run_all.sh 2 2 4 1   # shp
-bash run_all.sh 2 3 4 1   # pku
+CUDA_VISIBLE_DEVICES=6 bash run_all.sh 2 1 4 1 &   # hh
+CUDA_VISIBLE_DEVICES=5 bash run_all.sh 2 2 4 1 &   # shp
+CUDA_VISIBLE_DEVICES=1 bash run_all.sh 2 3 4 1 &   # pku
 
-# # ===============================
-# # Qwen 3B
-# # ===============================
-bash run_all.sh 3 1 4 1   # hh
-bash run_all.sh 3 2 4 1   # shp
-bash run_all.sh 3 3 4 1   # pku
+wait
+
+# ===============================
+# Qwen 3B
+# ===============================
+CUDA_VISIBLE_DEVICES=6 bash run_all.sh 3 1 4 1 &   # hh
+CUDA_VISIBLE_DEVICES=5 bash run_all.sh 3 2 4 1 &   # shp
+CUDA_VISIBLE_DEVICES=1 bash run_all.sh 3 3 4 1 &   # pku
+
+wait
 
 echo "🎉 ALL JOBS FINISHED"
